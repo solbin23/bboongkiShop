f119b8b35da58a1f2f73531cd7698914d8fc00ff
hongsolbin <solbin23@naver.com>
2022-09-06 23:17:56 +0900

    헤더수정 로그인수정 회원가입수정 아이템폼수정 MemberDto수정

--- src/main/java/com/taco/tacoshop/dto/MemberDto.java
+++ src/main/java/com/taco/tacoshop/dto/MemberDto.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.Email;
 
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -21,16 +22,16 @@ import javax.validation.constraints.Size;
 public class MemberDto {
 
 
-    @NotNull(message = "이메일형식으로 입력해주세요.")
+    @NotEmpty(message = "이메일형식으로 입력해주세요.")
     @Email
     private String email;
-    @NotNull(message = "이름은 필수 입력 값입니다.")
+    @NotEmpty(message = "이름은 필수로 입력해야합니다.")
     private String name;
-    @NotNull(message = "비밀번호는 필수 입력 값입니다.")
-    @Size(min = 5, max = 16, message = "비밀번호는 5자 이상, 16자 이하로 입력해주세요.")
+    @NotEmpty(message = "비밀번호는 필수로 입력해야합니다.")
+    @Size(min = 5, max = 16, message = "5자 이상, 16자 이하로 입력해주세요.")
     private String password;
 
-    @NotNull(message = "주소는 필수 입력 값입니다.")
+    @NotEmpty(message = "주소는 필수로 입력해야합니다.")
     private String address;
 
     @Builder
