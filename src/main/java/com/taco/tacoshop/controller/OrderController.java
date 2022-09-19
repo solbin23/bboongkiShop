e8b0c408f0d3e590352655b16483e710e79b6fb5
hongsolbin <solbin23@naver.com>
2022-09-19 17:57:23 +0900

    OrderController수정,  orderHistory 추가

--- src/main/java/com/taco/tacoshop/controller/OrderController.java
+++ src/main/java/com/taco/tacoshop/controller/OrderController.java
@@ -1,21 +1,25 @@
 package com.taco.tacoshop.controller;
 
 import com.taco.tacoshop.dto.OrderDto;
+import com.taco.tacoshop.dto.OrderHistoryDto;
 import com.taco.tacoshop.service.OrderService;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 
 @RequiredArgsConstructor
@@ -44,6 +48,20 @@ public class OrderController {
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+    }
+
+    @GetMapping(value = {"/orders","/orders/{page}"})
+    public String orderHistory(@PathVariable("page")Optional<Integer> page, Principal principal, Model model){
+
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 4);
+
+        Page<OrderHistoryDto> orderHistoryDtoList = orderService.getOrderList(principal.getName(), pageable);
+
+        model.addAttribute("orders", orderHistoryDtoList);
+        model.addAttribute("page", pageable.getPageNumber());
+        model.addAttribute("maxPage", 5);
+
+        return "order/orderHistory";
     }
 
 }
